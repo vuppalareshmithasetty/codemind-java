1578a25473e1418500a514eb72dc6f6605b6213b
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2023-08-11 18:55:20 +0530

    import java.util.Scanner; public class Sample{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n,x;         String a;         n=sc.nextInt();         for(int i=1;i<=n;i++)         {         a=sc.next();         x=Integer.parseInt(a,8);         System.out.println(Integer.toString(x,2));         }     } }

---
+++ Octal_to_Binary.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Sample{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n,x;
+        String a;
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+        a=sc.next();
+        x=Integer.parseInt(a,8);
+        System.out.println(Integer.toString(x,2));
+        }
+    }
+}
