e2148fb860e02734590d5562915e8de48ff91fd0
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2023-08-15 21:55:36 +0530

    import java.util.Scanner; public class Sample{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int a=sc.nextInt();         int b=sc.nextInt();         int c=(3*x)-(a+b);         System.out.print(c);     } }

---
+++ Average_weight.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Sample{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=(3*x)-(a+b);
+        System.out.print(c);
+    }
+}
