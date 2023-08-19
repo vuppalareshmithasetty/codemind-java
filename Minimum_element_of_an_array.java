4e50c02defe566605121edde26f2c4b12e294dd1
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2023-08-19 11:51:53 +0530

    import java.util.Scanner; public class Sample{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n =sc.nextInt();         int arr[] = new int[n];         for(int i=0;i<n;i++)         {            arr[i] = sc.nextInt();         }         int c=arr[0];         for(int i=0;i<n;i++)         {              if(c>arr[i]){                  c=arr[i];              }         }         System.out.println(c);     } }

---
+++ Minimum_element_of_an_array.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Sample{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++)
+        {
+           arr[i] = sc.nextInt();
+        }
+        int c=arr[0];
+        for(int i=0;i<n;i++)
+        {
+             if(c>arr[i]){
+                 c=arr[i];
+             }
+        }
+        System.out.println(c);
+    }
+}
