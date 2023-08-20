c3b8a2b8c9ae26faf2f05f13e15a97d2c1142381
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2023-08-20 11:02:12 +0530

    import java.util.Scanner; public class Sample{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n =sc.nextInt();         int odd=0,even=0;         int arr[] = new int[n];         for(int i=0;i<n;i++)         {            arr[i] = sc.nextInt();         }         for(int i=0;i<n;i++)         {            if(arr[i]%2==0){                even=even+arr[i];            }            else{                odd=odd +arr[i];            }         }         if(even>odd){             System.out.print(even-odd);         }         else{             System.out.print(odd-even);         }     } }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_elements.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Sample{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+        int odd=0,even=0;
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++)
+        {
+           arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+           if(arr[i]%2==0){
+               even=even+arr[i];
+           }
+           else{
+               odd=odd +arr[i];
+           }
+        }
+        if(even>odd){
+            System.out.print(even-odd);
+        }
+        else{
+            System.out.print(odd-even);
+        }
+    }
+}
