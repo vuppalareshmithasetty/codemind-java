6eec9b9cc20b880bd1abe9272314089316d26b1a
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2023-08-28 20:44:48 +0530

    import java.util.Scanner; public class sample{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n,x,y;         n=sc.nextInt();         for(int i=0;i<n;i++)         {             x=sc.nextInt();             y=sc.nextInt();             System.out.println(x+y);         }     } }

--- sum_of_the_given_numbers.java
+++ sum_of_the_given_numbers.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
-public class Sample{
+public class sample{
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+        int n,x,y;
+        n=sc.nextInt();
         for(int i=0;i<n;i++)
         {
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int s;
-        s=a+b;
-        System.out.println(s);
+            x=sc.nextInt();
+            y=sc.nextInt();
+            System.out.println(x+y);
         }
     }
 }
