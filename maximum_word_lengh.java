91d3552166cdb1964d787cb3779bbf0dd7442ca2
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2023-09-04 18:47:32 +0530

    import java.util.Scanner; public class sample{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         int c=0;         String[] arr=s.split(" ");         for(int i=0;i<arr.length;i++)         {             int z=arr[i].length();             if(c<=z)             {                 c=z;             }         }         System.out.print(c);     } }

---
+++ maximum_word_lengh.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class sample{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int c=0;
+        String[] arr=s.split(" ");
+        for(int i=0;i<arr.length;i++)
+        {
+            int z=arr[i].length();
+            if(c<=z)
+            {
+                c=z;
+            }
+        }
+        System.out.print(c);
+    }
+}
