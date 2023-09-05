29ee7bbcab976c602031d446a7c51de46d25ea73
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2023-09-05 22:32:30 +0530

    import java.util.*; public class sample{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         char[] arr=s.toCharArray();         System.out.print(arr.length);     } }

---
+++ Characters_in_a_String.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class sample{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        char[] arr=s.toCharArray();
+        System.out.print(arr.length);
+    }
+}
