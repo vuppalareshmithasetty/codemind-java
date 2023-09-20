5f2e3aca7a21e962159c278baae991debb9d2478
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2023-09-20 13:32:15 +0530

    import java.util.*; public class sample{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         int c=0;         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=1;i<n-1;i++)         {             if(a[i-1]%2!=0 && a[i+1]%2!=0)             {                 if(a[i]%2!=0)                 {                     c+=1;                 }             }         }         System.out.println(c);     } }

---
+++ count_the_odd_numbers_inbetween_odd_numbers.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class sample{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=1;i<n-1;i++)
+        {
+            if(a[i-1]%2!=0 && a[i+1]%2!=0)
+            {
+                if(a[i]%2!=0)
+                {
+                    c+=1;
+                }
+            }
+        }
+        System.out.println(c);
+    }
+}
